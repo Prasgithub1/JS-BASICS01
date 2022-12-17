a6b20c8ac1e11604649b11d507765633870a6e70
Prasgithub1 <parthiban.prasanna@gmai.com>
2022-12-17 13:37:54 +0400

    JS Commit

---
+++ js-basics01-.js
@@ -0,0 +1,29 @@
+var name = 'Adam' // string
+var age = 12 // number
+var email = 'shan@test.com'
+var address = '175, Dubai, UAE'
+var sum = 10+20 // 30
+var x = 'a'+10 // 'a10'
+var foo = 'hello' + 'world' // 'hello world
+var isHuman = true // boolean
+isHuman = false //
+var x1 = 1
+var y1 = 2
+var z = x1 + y1 // 3
+console.log('Hello World')
+console.log(x)
+console.log(10+40)
+
+
+function isBoolean(number) {
+    let i = 5000; i <= 9999; i++
+    {
+    if (num % i === 5000) {
+    isBoolean = false;
+    } else {
+    isBoolean = true;
+    }
+    }
+    return isBoolean;
+    }
+    console.log(isBoolean)
---
+++ js-basics03-.js
@@ -0,0 +1,32 @@
+var name = "Anush"
+
+var age = "32"
+
+var email = "anush@ymail.com"
+
+var address = "123, Lake Point Tower, JLT, Dubai"
+
+var sum = 30+56 //86
+
+var x = "a"+5 //a5
+
+var tag = "hello"+"prasanna" // hello prasanna
+
+var isHuman = true //boolean
+
+isHuman = false
+
+var isVaccinated = false
+
+var x1 = 1
+var y1 = 2
+
+var z1 = x1 + y1
+var z1 = z1 - x1
+
+console.log("Hello Prasanna")
+console.log(x)
+///go to
+
+////hello
+
